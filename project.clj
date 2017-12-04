bd6d9258a41fbdd1265c4acfd31878b94ef4b9df
francesc <francesc.guillen@gmail.com>
2017-12-04 22:16:23 +0100

    project

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject aoc "0.1.0-SNAPSHOT"
+  :description "aoc"
+  :url "http://adventofcode.com"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
