2416c826c70751208a4fd08de0b7ca0c59b13b36
francesc <francesc.guillen@gmail.com>
2017-12-07 09:21:59 +0100

    day7 2

--- src/aoc/2017/day7/recursive_circus.clj
+++ src/aoc/2017/day7/recursive_circus.clj
@@ -1,7 +1,6 @@
 (ns aoc.2017.day7.recursive-circus
   (:require [clojure.test :refer [deftest is]]))
 
-
 (defn parse-line [line]
   (let [tok (clojure.string/split line #" -> ")
         sub (if (nil? (second tok)) []
@@ -12,11 +11,8 @@
                     (drop 1)
                     drop-last
                     (apply str)
-                    read-string
-                    )
-        ]
-    {name {:w weight :sub sub}})
-  )
+                    read-string)]
+    {name {:w weight :sub sub}}))
 
 (defn father [data]
   (let [programs (-> data
@@ -32,12 +28,9 @@
 
 (deftest parse-program
   (is (= {"p1" {:w 25 :sub ["p2" "p3"]}}
-         (parse-line "p1 (25) -> p2, p3")
-         ))
+         (parse-line "p1 (25) -> p2, p3")))
   (is (= {"p2" {:w 25 :sub []}}
-         (parse-line "p2 (25)")
-         )))
-
+         (parse-line "p2 (25)"))))
 
 ;; load input
 (def input (reduce #(merge (parse-line %2) %1) {}
@@ -45,4 +38,27 @@
 
 ;; sol1
 (father input)
+
+(declare get-sub-weights)
+
+(defn get-weight [input key]
+  (let [current (input key)]
+    {key 
+     (+ (:w current) (apply + (flatten (map vals (get-sub-weights input key)))))}))
+
+(defn get-sub-weights [input key]
+  (let [current (input key)]
+    (if (empty? (:sub current))
+      []
+      (map (partial get-weight input) (:sub current)))))
+
+;; sol2 got to the solution while exploring the problem.
+(get-sub-weights input "svugo")
+(get-sub-weights input (second (:sub (input "svugo"))))
+(get-sub-weights input "yruivis")
+(get-sub-weights input "sphbbz")
+(input "sphbbz")
+(- 1161 9 )
+(def fixed-input (assoc-in input ["sphbbz" :w] 1152))
+(get-sub-weights fixed-input "svugo")
 
