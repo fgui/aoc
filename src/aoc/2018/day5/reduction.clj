667a344e147d597f976015d13f4ff7576f627b66
francesc <francesc.guillen@gmail.com>
2018-12-05 19:59:28 +0100

    day5 make it faster

--- src/aoc/2018/day5/reduction.clj
+++ src/aoc/2018/day5/reduction.clj
@@ -1,29 +1,28 @@
 (ns aoc.2018.day5.reduction)
 
-
 (defn parse-input []
   (vec (drop-last (slurp "resources/2018/day5.txt"))))
 
-(parse-input)
 
 (defn abs [n] (max n (- n)))
 
 (defn reduct [pol]
   (let [new-pol
-        (reduce  (fn [col next]
-                   (let [prev (last col)]
-                     (if (and
-                          (not (nil? prev))
-                          (= 32 (abs (- (int prev)
-                                     (int next)))
-                                 ))
-                       (vec (drop-last col))
-                       (conj col next)
-                       )
-                     )
-                   )
-                 [] pol
-                 )]
+        (reverse
+         (reduce  (fn [col next]
+                    (let [prev (first col)]
+                      (if (and
+                           (not (nil? prev))
+                           (= 32 (abs (- (int prev)
+                                         (int next)))
+                              ))
+                        (rest col)
+                        (conj col next)
+                        )
+                      )
+                    )
+                  '() pol
+                  ))]
     (println (count pol))
     (if (= (count pol) (count new-pol))
       new-pol
@@ -31,24 +30,18 @@
       )
     ))
 
-(def res1 (reduct (parse-input)))
 
-(def answer1 (count res1))
+(defn answer1 [] (count (reduct (parse-input))))
 
-(reduct (vec "dabAcCaCBAcCcaDA"))
-(reduct (vec "abBA"))
-
-(count (vec "abBA"))
-
-(def units
-  (map (fn [x] #{(char x) (char (- x 32))})
-       (range (int \a) (int \z))))
+(answer1)
 
 (defn answer2 []
-  (apply min (reduce
-        #(conj %1 (count (reduct
-                          (filter (fn [v] (not (contains? %2 v))) res1))))
-       []
-       units)))
+  (let [units (map (fn [x] #{(char x) (char (- x 32))})
+                   (range (int \a) (int \z)))]
+    (apply min (reduce
+                #(conj %1 (count (reduct
+                                  (filter (fn [v] (not (contains? %2 v))) (reduct (parse-input))))))
+                []
+                units))))
 
 (answer2)
