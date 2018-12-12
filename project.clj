e74ea39998e3e224511cbd327c70f5b9e7b7429b
francesc <francesc.guillen@gmail.com>
2018-12-12 06:03:09 +0100

    day9 part 2 - own data structure and increase memory jvm.

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[digest "1.4.8"]
-                 [org.clojure/clojure "1.8.0"]])
+                 [org.clojure/clojure "1.9.0"]]
+  :jvm-opts ["-Xmx3g" "-server"] )
--- src/aoc/2018/day9/marbles.clj
+++ src/aoc/2018/day9/marbles.clj
@@ -1,5 +1,6 @@
 (ns aoc.2018.day9.marbles)
 
+;; first attempt persistent datastrutures not to much tought, too slow
 (defn play [[players board current-idx] num-marble]
   (let [player (mod (dec num-marble) (count players))
         next-pos (mod (+ current-idx 2) (count board))
@@ -18,6 +19,13 @@
        next-pos
        ])))
 
+(defn game [num-players last-ball]
+  (reduce play [(vec (repeat num-players 0)) [0] 0] (range 1 (inc last-ball))))
+
+(defn max-score [num-players last-ball]
+  (apply max (first (game num-players last-ball))))
+
+;; second attempt ArrayList delete too slow
 (defn play-al [[players  board current-idx] num-marble]
   (let [player (mod (dec num-marble) (count players))
         next-pos (mod (+ current-idx 2) (.size board))
@@ -40,6 +48,15 @@
          next-pos
          ]))))
 
+(defn game-al [num-players last-ball]
+  (let [^java.util.ArrayList board (new java.util.ArrayList)]
+    (.add board 0 0)
+    (reduce play-al [(vec (repeat num-players 0)) board 0] (range 1 (inc last-ball)))))
+
+(defn max-score-al [num-players last-ball]
+  (apply max (first (game-al num-players last-ball))))
+
+;; thirt LinkedList still not fast enough
 (defn play-ll [[players  board current-idx] num-marble]
   (let [player (mod (dec num-marble) (count players))
         next-pos (mod (+ current-idx 2) (.size board))
@@ -62,93 +79,81 @@
          next-pos
          ]))))
 
-(defn game [num-players last-ball]
-  (reduce play [(vec (repeat num-players 0)) [0] 0] (range 1 (inc last-ball))))
-
-(defn game-al [num-players last-ball]
-  (let [^java.util.ArrayList board (new java.util.ArrayList)]
-    (.add board 0 0)
-    (reduce play-al [(vec (repeat num-players 0)) board 0] (range 1 (inc last-ball)))))
-
 (defn game-ll [num-players last-ball]
   (let [^java.util.LinkedList board (new java.util.LinkedList)]
     (.add board 0 0)
     (reduce play-ll [(vec (repeat num-players 0)) board 0] (range 1 (inc last-ball)))))
 
-(defn max-score-al [num-players last-ball]
-  (apply max (first (game-al num-players last-ball))))
-
 (defn max-score-ll [num-players last-ball]
   (apply max (first (game-ll num-players last-ball))))
 
-(defn max-score [num-players last-ball]
-  (apply max (first (game num-players last-ball))))
 
 
 
-;;(max-score-ll 452 7125000)
-;; optimization 1 use ArrayList java instead of clojure
-(defn answer1 []
-  (max-score-al 452 71250)
-  )
 
-(defn answer2 []
-  (max-score-al 452 (* 100 71250)))
 
 (defn initial-board []
   (let [initial-board
-        {:prev (atom nil)
-         :next (atom nil)
-         :value 0}
+        [(atom nil)
+         (atom nil)
+         0]
         ]
-    (reset! (:prev initial-board) initial-board)
-    (reset! (:next initial-board) initial-board)
+    (reset! (first initial-board) initial-board)
+    (reset! (second initial-board) initial-board)
     initial-board))
 
-
+;; last attempt got both solutions with this.
+;; jvm memory set to 3Gb and server.
+;; fast access to current node which is the board itself
+;; fast access to siblings and fast insert and delete.
+;; Used a vector [previous next value]
 (defn play-board [[players board] num-marble]
   (let [player (mod (dec num-marble) (count players))
         special-ball (zero? (mod num-marble 23))]
     (if (zero? (mod num-marble 10000))
       (println num-marble))
     (if special-ball
-      (let [node @(:prev
-                   @(:prev
-                     @(:prev
-                       @(:prev
-                         @(:prev
-                           @(:prev
-                             @(:prev board)))))))
-            val (:value node)
+      (let [node @(first
+                   @(first
+                     @(first
+                       @(first
+                         @(first
+                           @(first
+                             @(first board)))))))
+            val (last node)
             ]
-        (reset! (:next @(:prev node)) @(:next node))
-        (reset! (:prev @(:next node)) @(:prev node))
+        (reset! (second @(first node)) @(second node))
+        (reset! (first @(second node)) @(first node))
         [(update players player + num-marble val)
-         @(:next node)]
+         @(second node)]
         )
-      (let [node @(:next board)
-            new-node {:value num-marble
-                      :prev (atom node)
-                      :next (atom @(:next node))}]
-        (reset! (:prev @(:next node)) new-node)
-        (reset! (:next node) new-node)
+      (let [node @(second board)
+            new-node [
+                      (atom node)
+                      (atom @(second node))
+                      num-marble]]
+        (reset! (first @(second node)) new-node)
+        (reset! (second node) new-node)
         [players
          new-node
-         ])
-      ))
-  )
+         ]))))
 
 
-(defn game2 [num-players last-ball]
+(defn game-board [num-players last-ball]
   (reduce play-board [(vec (repeat num-players 0)) (initial-board)] (range 1 (inc last-ball))))
 
-(defn max-score2 [num-players last-ball]
-  (apply max (first (game2 num-players last-ball))))
+(defn max-score-board [num-players last-ball]
+  (apply max (first (game-board num-players last-ball))))
 
 
-;;(play-board [[0 0] (initial-board)] 1)
+(defn answer1 []
+  (max-score-board 452 71250)
+  )
 
-(comment 
+(defn answer2 []
+  (max-score-board 452 (* 100 71250)))
+
+(comment
   (max-score 9 25)
   (max-score2 9 25)
   (game 9 25)
@@ -160,10 +165,8 @@
   (max-score2 10 1618)
   (max-score 30 5807)
   (max-score-ll 30 5807)
+  (max-score2 452 71250)
   (max-score2 452 (* 100 71250))
   )
-;; optimization 2 TODO
-;; use the right type of struture for add retrieve and delete
-;; create my own datastructure to reprent the problem
-;; circular list with previous next and keep a reference to pointer.
+
 
